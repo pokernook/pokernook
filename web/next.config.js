bf6b9f59ea96b3d0112e9fdf6e6ff38c96b3f527
Ben Walker <bwalke05@gmail.com>
2021-04-05 12:00:55 -0400

    Initial lobby setup (#15)
    * Add games index
    
    * Add multiplayer endpoint rewrite
    
    * Add boardgame.io to webproject
    
    * Fix multiplayer rewrites

--- web/next.config.js
+++ web/next.config.js
@@ -8,6 +8,12 @@ module.exports = {
   rewrites: async () => {
     return IS_PRODUCTION
       ? []
-      : [{ source: "/api/graphql", destination: "http://localhost:4000" }];
+      : [
+          { source: "/api/graphql", destination: "http://localhost:4000" },
+          {
+            source: "/api/multiplayer/:path*",
+            destination: "http://localhost:8000/:path*",
+          },
+        ];
   },
 };
