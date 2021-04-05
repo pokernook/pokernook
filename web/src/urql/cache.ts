c9fefbfbcb6880883cb075f9eb0943bd59f099b7
Ben Walker <bwalke05@gmail.com>
2021-04-05 13:53:49 -0400

    Update cache on friendship delete

--- web/src/urql/cache.ts
+++ web/src/urql/cache.ts
@@ -115,5 +115,21 @@ export const updates: Partial<UpdatesConfig> = {
       );
       cache.invalidate("Query", "friendships");
     },
+
+    friendshipDelete: (result, _args, cache) => {
+      cache.updateQuery(
+        { query: graphql.FriendshipsDocument },
+        (data: graphql.FriendshipsQuery | null) => {
+          const castResult = result as graphql.FriendshipDeleteMutation;
+          if (data?.me) {
+            const removedIndex = data.me.friendships.findIndex(
+              (f) => f.id === castResult.friendshipDelete?.id
+            );
+            data.me.friendships.splice(removedIndex, 1);
+          }
+          return data;
+        }
+      );
+    },
   },
 };
