65289651476b89385631532ec411a8763aafbec9
Ben Walker <bwalke05@gmail.com>
2021-04-11 00:02:57 -0400

    Tic tac toe (#41)
    * Initial server setup
    
    * Switch to a simpler game

--- multiplayer/server.ts
+++ multiplayer/server.ts
@@ -1,18 +1,19 @@
 import { PostgresStore } from "bgio-postgres";
 import { Server } from "boardgame.io/server";
 
+import { DemoNumbers } from "./games";
+
 const DATABASE_URL = process.env.DATABASE_URL || "";
 const PORT = parseInt(process.env.PORT || "8000", 10);
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 const server = Server({
   db: new PostgresStore(DATABASE_URL, {
     dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      ssl: IS_PRODUCTION ? { rejectUnauthorized: false } : undefined,
     },
   }),
-  games: [],
+  games: [DemoNumbers],
 });
 
 server
